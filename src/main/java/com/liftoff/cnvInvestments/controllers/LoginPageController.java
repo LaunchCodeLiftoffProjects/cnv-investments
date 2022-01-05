59f6ee94bbdf19700285f2e2f6f580db0f08ec1f
Christi <christi.tahk@gmail.com>
2022-01-04 20:35:23 -0600

    added login page

---
+++ src/main/java/com/liftoff/cnvInvestments/controllers/LoginPageController.java
@@ -0,0 +1,19 @@
+package com.liftoff.cnvInvestments.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+
+public class LoginPageController {
+
+    @GetMapping("")
+    public String login() {
+
+        return "login";
+    }
+
+
+}
