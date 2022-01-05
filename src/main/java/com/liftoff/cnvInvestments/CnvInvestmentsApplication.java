c775c0f816e3a8ee3a3d66edeae2677ac2d96c5b
niranjan <sr.niranjan@gmail.com>
2022-01-04 19:47:41 -0600

    set up the project

---
+++ src/main/java/com/liftoff/cnvInvestments/CnvInvestmentsApplication.java
@@ -0,0 +1,13 @@
+package com.liftoff.cnvInvestments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnvInvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnvInvestmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liftoff/cnvInvestments/CnvInvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liftoff.cnvInvestments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnvInvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
