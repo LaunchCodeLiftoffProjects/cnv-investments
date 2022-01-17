b10042fa5993eb83db2d2d03813101db7eb73d67
Latisha Vaughm <vic86rous@gmail.com>
2022-01-17 13:21:36 -0600

    updated homepage

--- src/main/java/com/liftoff/cnvInvestments/controllers/HomePageController.java
+++ src/main/java/com/liftoff/cnvInvestments/controllers/HomePageController.java
@@ -1,9 +1,16 @@
 package com.liftoff.cnvInvestments.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class HomePageController {
 
-
+    @GetMapping(value = "")
+    public String loginPage (Model model) {
+        return "authentication";
+    }
 }
