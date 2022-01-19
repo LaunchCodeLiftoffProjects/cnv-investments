0feda2d5fe5530a88ad0b7b42ca2f71baafde35c
Latisha Vaughm <vic86rous@gmail.com>
2022-01-18 22:05:04 -0600

    updated homepage controller , added the fragment page to all the templates and updated the login page

--- src/main/java/com/liftoff/cnvInvestments/controllers/HomePageController.java
+++ src/main/java/com/liftoff/cnvInvestments/controllers/HomePageController.java
@@ -2,15 +2,22 @@ package com.liftoff.cnvInvestments.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+
 @Controller
-@RequestMapping
 public class HomePageController {
 
-    @GetMapping(value = "")
-    public String loginPage (Model model) {
-        return "authentication";
+    @RequestMapping(value = "")
+    public String login(Model model) {
+
+        HashMap<String, String> actionChoices = new HashMap<>();
+        actionChoices.put("investors", "Investors");
+        actionChoices.put("fund-managers", "Fund Managers");
+
+        model.addAttribute("actions", actionChoices);
+
+        return "login-page";
     }
 }
