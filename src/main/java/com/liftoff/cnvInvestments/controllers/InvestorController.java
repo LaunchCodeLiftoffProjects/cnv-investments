c2b36b0e7e33032db63a2186050765538b6de91a
Latisha Vaughm <vic86rous@gmail.com>
2022-01-19 01:40:16 -0600

    minor adjustment to investor controller

--- src/main/java/com/liftoff/cnvInvestments/controllers/InvestorController.java
+++ src/main/java/com/liftoff/cnvInvestments/controllers/InvestorController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("investors")
-
+@RequestMapping(value = "investors")
 public class InvestorController {
+
 
     @GetMapping
     public String InvestorPage () {
