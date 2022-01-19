2f699db69642b8980d5f4ec108e85b347c922f6f
Latisha Vaughm <vic86rous@gmail.com>
2022-01-19 00:48:43 -0600

    added fund manager & investor controller

--- src/main/java/com/liftoff/cnvInvestments/controllers/InvestorController.java
+++ src/main/java/com/liftoff/cnvInvestments/controllers/InvestorController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("summary")
-public class SummaryInvestorController {
+@RequestMapping("investors")
+
+public class InvestorController {
 
     @GetMapping
-    public String displaySummaryPage() {
-
-        return "investor-summary-page";
+    public String InvestorPage () {
+        return "investor-portfolio-page";
     }
 }
