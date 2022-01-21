08c24f558d619a87e1be13c86b339018f11e86a1
Christi <christi.tahk@gmail.com>
2022-01-20 18:16:54 -0600

    register page redirected to login page

--- src/main/java/com/liftoff/cnvInvestments/AuthenticationFilter.java
+++ src/main/java/com/liftoff/cnvInvestments/AuthenticationFilter.java
@@ -37,21 +37,17 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
                              HttpServletResponse response,
                              Object handler) throws IOException {
 
-        // Don't require sign-in for whitelisted pages
         if (isWhitelisted(request.getRequestURI())) {
-            // returning true indicates that the request may proceed
             return true;
         }
 
         HttpSession session = request.getSession();
         User user = authenticationController.getUserFromSession(session);
 
-        // The user is logged in
         if (user != null) {
             return true;
         }
 
-        // The user is NOT logged in
         response.sendRedirect("/login");
         return false;
     }
--- src/main/java/com/liftoff/cnvInvestments/WebApplicationConfiguration.java
+++ src/main/java/com/liftoff/cnvInvestments/WebApplicationConfiguration.java
@@ -6,13 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebApplicationConfiguration implements WebMvcConfigurer {
 
-    // Create spring-managed object to allow the app to access our filter
     @Bean
     public AuthenticationFilter authenticationFilter() {
         return new AuthenticationFilter();
     }
 
-    // Register the filter with the Spring container
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor( authenticationFilter() );
