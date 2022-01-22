5f1d4a8572bd12d86477e1c779c71e7e45bcf6b4
niranjan <sr.niranjan@gmail.com>
2022-01-22 05:52:18 -0600

    created new branch for investor transactions page and created a controller for it

---
+++ src/main/java/com/liftoff/cnvInvestments/controllers/TransactionsInvestorController.java
@@ -0,0 +1,15 @@
+package com.liftoff.cnvInvestments.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("transactions")
+
+public class TransactionsInvestorController {
+
+    @GetMapping
+    public String displayTransactionsPage() { return "investor-transactions-page"; }
+
+}
